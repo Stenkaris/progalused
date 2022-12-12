80b17ef512ac750f96dd9376d3c1cfbf5ff6207f
Sten <sten.karis@voco.ee>
2022-12-12 14:06:56 +0200

    KONTROLLTÖÖ

---
+++ KONTROLTÖÖ.py
@@ -0,0 +1,60 @@
+import datetime
+
+isikukood = input("Palun sisestage oma isikukood: ")
+
+print("Teie isiklik kood on: ", isikukood)
+
+isikukood = "12051998"
+
+
+täna = kuupäev - kellaaeg.kuupäev.täna()
+
+päev = int(isikukood[0:2])
+kuu = int(isikukood[2:4])
+aasta = int(isikukood[4:8])
+
+sünni_kuupäev = datetime.date(aasta, kuu, päev)
+
+vanus = tänane_sünni_kuupäev
+
+sünni_aasta = tänane.aasta - vanus.päevad // 365
+
+print(f"Sünniaasta: {year_of_birth}")
+
+isikukood = "12051998"
+
+tähemärkide_arv = len(isikukood)
+print(f"Tähemärkide arv: {num_chars}")
+
+ühed_arv = personaalne_id.count("1")
+print(f"Ühe '1' esinemiste arv: {num_ones}")
+
+esimene_üheksa_indeks = isiklik_id.indeks("9")
+print(f"9 esmakordse esinemise indeks: {first_nine_index}")
+
+has_zero = "0" in isikukood
+print(f"Sisaldab '0': {has_zero}")
+
+sõna_number = "123"
+
+number = int(sõna_arv)
+
+print(f"{word_number} teisendatud täisarvuks: {number}")
+
+string = "123"
+
+number = int(string)
+
+print(f"{string} teisendatud täisarvuks: {number}")
+
+isikukood = "120519987"
+
+esimene_kümme_numbrit = isikukood[0:10]
+kontroll_number = isikukood[-1]
+
+arvutatud_kontrolli_arv = "7"
+
+if kontrolli_arv == arvutatud_kontrolli_arv:
+    print("Isikukood on kehtiv")
+else:
+    print("Isikukood ei kehti")
